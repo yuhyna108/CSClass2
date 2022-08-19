9a48bbb1f71265adb105d383503dc90b1e8b45ac
yuhyna108 <s2012@e-mirim.hs.kr>
2022-08-19 11:07:54 +0900

    #24-3. out 키워드

--- CSclass2/Program.cs
+++ CSclass2/Program.cs
@@ -18,10 +18,37 @@ namespace CSclass2
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantedDouble.Value);
 
+
             Products p = new Products();
             Console.WriteLine("오늘의 점심 메뉴는 " + p[3]+"입니다.");
             p[2] = "단무지";
-            Console.WriteLine("오늘의 점심 메뉴는 " + p[2] + "입니다. ");
+            //Console.WriteLine("오늘의 점심 메뉴는 " + p[2] + "입니다. ");
+
+
+            Console.WriteLine("숫자 입력 : ");
+            //int output;
+            bool result=int.TryParse(Console.ReadLine(), out int output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자 " + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요!");
+            }
+            int x = 0;
+            int y = 0;
+            int vx=1;
+            int vy=1;
+            Console.WriteLine("현재 좌표 : (" + x + "," + y + ")");
+            NextPos(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표 : ("+ x + "," + y + ")");
+        }
+
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
         }
     }
 }
