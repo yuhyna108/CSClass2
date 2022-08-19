64c8a93cac39d87efc969e041fa35468171ea93f
yuhyna108 <s2012@e-mirim.hs.kr>
2022-08-19 10:35:24 +0900

    #1 Generic 실습 wanted클래스 생성, string int double 클래스 변수를 선언함

--- CSclass2/Program.cs
+++ CSclass2/Program.cs
@@ -10,6 +10,13 @@ namespace CSclass2
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("String");
+            Wanted<int> wantedInt = new Wanted<int>(52273);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
